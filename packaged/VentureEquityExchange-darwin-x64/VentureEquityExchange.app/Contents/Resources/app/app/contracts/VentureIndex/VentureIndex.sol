d25dc1b0f8751eac2f8985b5ba6e9fece9ecdc41
Ryan Tate <ryan@ventureequity.co>
2016-01-12 15:58:24 -0800

    removing packaged folder

--- packaged/VentureEquityExchange-darwin-x64/VentureEquityExchange.app/Contents/Resources/app/app/contracts/VentureIndex/VentureIndex.sol
+++
@@ -1,8 +0,0 @@
-contract VenturesIndex {
-    address[] public index;
-    
-    function AddVenture(address venture) public returns (bool){
-        index.push(venture);
-        return true;
-    }
-}
